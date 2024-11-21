0395ff18c609a7b380eef95709806b197359403a
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2024-11-20 21:00:28 -0500

    ok got a tour

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns=[
-    path("", views.home, name="home")
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("todos/", views.todos, name='todos'),
 ]
